dfabfb22e630da7470f7a860a1facfa78b3666cc
marybaum <marybaum@marybaum.com>
2022-11-10 17:20:56 -0600

    Initial commit

---
+++ wp-content/plugins/wordpress-beta-tester/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'afragen/wordpress-beta-tester',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'd6510bf85f6f4323188a11ccd8d1edd4f49a3154',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'afragen/wordpress-beta-tester' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'd6510bf85f6f4323188a11ccd8d1edd4f49a3154',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'wp-cli/wp-config-transformer' => array(
+            'pretty_version' => 'v1.3.0',
+            'version' => '1.3.0.0',
+            'reference' => '2e90eefc6b8f5166f53aa5414fd8f1a572164ef1',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../wp-cli/wp-config-transformer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
